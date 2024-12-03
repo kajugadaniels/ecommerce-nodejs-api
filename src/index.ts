29f9946f6bbb2e4df5180505cdfeba921fdf46ed
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-03 19:18:13 +0200

    Add user route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import { user_route } from './routes/user.route'
 const app = express()
+
+app.use("/api/users", user_route)
 
 app.listen(8000, () => {
     console.log("App running on port 8000")
---
+++ src/routes/user.route.ts
@@ -0,0 +1,4 @@
+import express from "express"
+export const user_route = express.Router();
+
+user_route.get("/allusers")
