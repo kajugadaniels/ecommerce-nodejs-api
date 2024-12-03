167acfa8e1fac4d513d5b954825e6ae4623bdb45
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-03 19:37:04 +0200

    Setup user controller

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import { all_users } from "../controllers/user.controller"
 export const user_route = express.Router();
 
-user_route.get("/allusers")
+user_route.get("/allusers", all_users)
