17755ded84c85cfe39d780246f85893c56585c86
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-04 10:57:04 +0200

    New changes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,3 +3,7 @@ import { Request, Response } from "express";
 export const all_users = async (req: Request, res: Response) => {
     res.send("Hello");
 };
+
+export const create_user = async (req: Request, res: Response) => {
+    res.send("Hello");
+};
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,7 @@
 import express from "express"
-import { all_users } from "../controllers/user.controller"
+import { all_users, create_user } from "../controllers/user.controller"
 export const user_route = express.Router();
 
 user_route.get("/allusers", all_users)
+
+user_route.get("/add-new-user", create_user)
