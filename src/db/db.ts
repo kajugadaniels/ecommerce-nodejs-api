aa2ad896bd813d5feecd95bf98907e0f8d059310
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-09 12:39:19 +0200

    Configured dotenv for environment variable management and updated index.ts and db.ts accordingly.

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,16 +1,19 @@
 import { createConnection } from 'typeorm';
 import { User } from '../entities/user.entity';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = createConnection({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'enso',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [User],
     synchronize: true,
-    // logging: true,
+    logging: false,
 })
-  .then(() => console.log("Database connected successfully"))
-  .catch(err => console.error("Database connection failed", err));
+.then(() => console.log("Database connected successfully"))
+.catch(err => console.error("Database connection failed", err));
