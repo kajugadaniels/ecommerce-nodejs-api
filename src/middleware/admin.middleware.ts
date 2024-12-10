1b87303a48916c35f9a87358d0ff17f60437310d
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-12-10 16:10:57 +0200

    Updated isAdmin middleware to verify accountType using the updated AuthRequest interface.~

--- src/middleware/admin.middleware.ts
+++ src/middleware/admin.middleware.ts
@@ -1,8 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+import { Response, NextFunction } from 'express';
 import { AuthRequest } from './auth.middleware';
+import { AccountType } from '../entities/user.entity'; // Import AccountType
 
 export const isAdmin = (req: AuthRequest, res: Response, next: NextFunction) => {
-    if (req.user && req.user.accountType === 'Admin') {
+    if (req.user && req.user.accountType === AccountType.ADMIN) {
         next();
     } else {
         res.status(403).json({ message: 'Forbidden: Admins only' });
